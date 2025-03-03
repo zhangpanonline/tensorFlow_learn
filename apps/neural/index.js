7951947e16b673c761413cc6f0a68be5aa352c0e
zhangpan <zhangpan.online@outlook.com>
2025-03-03 22:01:17 +0800

    123

---
+++ apps/neural/index.js
@@ -0,0 +1,9 @@
+import * as tf from '@tensorflow/tfjs';
+import { TRAINING_DATA } from 'https://storage.googleapis.com/jmstore/TensorFlowJS/EdX/TrainingData/real-estate-data.js';
+
+const { inputs: INPUTS, outputs: OUTPUTS  } = TRAINING_DATA
+
+tf.util.shuffleCombo(INPUTS, OUTPUTS)
+
+const INPUTS_TENSOR = tf.tensor2d(INPUTS)
+const OUTPUTS_TENSOR = tf.tensor1d(OUTPUTS)
